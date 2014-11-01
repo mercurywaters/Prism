822cae694380c408ee9875a11455bbd3d34a93db
Faheem Chaudhary <faheemc@faheemc-mbp15.local>
2014-11-01 14:34:09 -0700

    Initial Commit

---
+++ Prism/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  Prism
+//
+//  Created by Faheem Chaudhary on 11/1/14.
+//  Copyright (c) 2014 Faheem Inayat. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    @IBOutlet weak var window: NSWindow!
+    
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+    
+}
